e10206760ad3458f436e50f14ffe4fd02448f2c4
DanielDlc <dlc.engcomputacao@gmail.com>
2022-03-29 08:12:14 -0300

    criação de variáveis com Csharp

---
+++ Fundação/01_Variáveis/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Variaveis
+{
+    class Variaveis
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Olá, Mundo!";
+            Console.WriteLine(texto);
+        }
+    }
+}
