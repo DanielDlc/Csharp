947b32118bb26b3e8ed05a0a6d708579f5373ef7
DanielDlc <dlc.engcomputacao@gmail.com>
2022-03-31 10:28:40 -0300

    switch

---
+++ Fundação/03_Switch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Switch 
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            
+            string nome = "Paulo";
+            switch (nome)
+            {
+                case "Diego": Console.WriteLine("Diego é o player"); break;
+                case "Jean": Console.WriteLine("Jean é o player"); break;
+                case "Paulo": Console.WriteLine("Paulo é o player"); break;
+                case "Rogerio": Console.WriteLine("Rogerio é o player"); break;
+                case "Daniel": Console.WriteLine("Daniel é o player"); break;
+                default: Console.WriteLine("Não encontrei o player"); break;
+            }
+        }
+    }
+}
