56abcec9109c3e84c24d6faa7c6f48dae37fdaf1
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-01 08:27:24 -0300

    do while

---
+++ Fundação/06_do_while/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // do (faça), executa primeiro e depois analisa a condição.
+            int valor = 0;
+            do  
+            {
+                Console.WriteLine(valor);
+                valor ++;
+            } while (valor <= 5);
+        }
+    }
+}
