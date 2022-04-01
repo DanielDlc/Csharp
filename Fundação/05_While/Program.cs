0274b8913bd35bcb7ce01cfc136c5b8e82b98fc3
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-01 08:22:18 -0300

    while

--- Fundação/05_While/Program.cs
+++ Fundação/05_While/Program.cs
@@ -8,7 +8,7 @@ namespace While
         {
             // contador de 1 até 10
             int valor = 1;
-            while(valor <= 10)
+            while(valor <= 10)  // while, analisa a condição antes de executar.
             {
                 Console.WriteLine(valor);
                 valor ++;
