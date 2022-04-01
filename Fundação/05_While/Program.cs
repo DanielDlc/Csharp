22a0608ce57a3b4337ca51a0a2b282b4793a4a24
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-01 08:13:52 -0300

    while

---
+++ Fundação/05_While/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)        
+        {
+            // contador de 1 até 10
+            int valor = 1;
+            while(valor <= 10)
+            {
+                Console.WriteLine(valor);
+                valor ++;
+            }
+        }
+    }
+}
