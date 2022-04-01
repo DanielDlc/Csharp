584202c10d90295f701655d4554d58e547a512c2
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-01 08:02:45 -0300

    laço for

---
+++ Fundação/04_Laço_For/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RepetiçãoFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // contagem regressiva de 10 até 1
+            int contador;
+            for ( contador = 10; contador >= 1; contador--)
+            {
+                Console.WriteLine(contador);
+            }
+        }
+    }
+}
