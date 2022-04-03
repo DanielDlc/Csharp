39cafc99039af374d6e8e87113384622468a50a3
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-03 09:52:57 -0300

    Value types

---
+++ Fundação/08_TipoDeValor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Valor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 38;
+            int numeroFixo = numero;  // criando uma cópia do número 38 na memória.
+            Console.WriteLine(numero);
+            Console.WriteLine(numeroFixo);
+
+            Console.WriteLine("--------");
+
+            numero = 40;  // mudando número 38 para o número 40
+            Console.WriteLine($"O número modificado possui o valor {numero}");
+            Console.WriteLine($"O número fixo continua no valor {numeroFixo}");
+
+        }
+    }
+}
---
+++ Fundação/09_TipoDeReferencia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Valor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new string[2];
+            arr[0] = "item1";
+            var arr2 = arr;
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
+            
+            Console.WriteLine("-----");
+            
+            arr[0] = "item2";
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
+        }
+    }
+}
