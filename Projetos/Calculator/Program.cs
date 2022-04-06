675abdb3ee6ec35847ed6f64648fb37a38d746c2
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-06 09:27:06 -0300

    calculadora Readline e Writeline pronto

---
+++ Projetos/Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Calculator
+{
+    class Porgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());  // convertendo string para o tipo float
+
+            Console.WriteLine(v1);
+        }
+    }    
+}
