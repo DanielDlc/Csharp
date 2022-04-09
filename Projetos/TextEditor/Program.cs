fd615b27faff61a2081188039fe437116dcffe5d
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-09 07:44:18 -0300

    abrindo arquivo de texto

--- Projetos/TextEditor/Program.cs
+++ Projetos/TextEditor/Program.cs
@@ -26,7 +26,19 @@
 
             static void Abrir()
             {
+                Console.Clear();
+                Console.WriteLine("Qual o caminho do arquivo?");
+                string path = Console.ReadLine();
 
+                using(var file = new StreamReader(path))
+                {
+                    string text = file.ReadToEnd();
+                    Console.WriteLine(text);
+                }
+                
+                Console.WriteLine("");
+    	        Console.ReadLine();
+                Menu();
             }
 
             static void Editar()
