8186be05d04b4da1cf2477eec282e00d029f3ed1
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-11 06:42:17 -0300

    usando Format

--- Fundação/12_String/Program.cs
+++ Fundação/12_String/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             var price = 400.9;
-            var texto = "o preço do produto é " + price + " apenas na promoção";
-
+            //var texto = "o preço do produto é " + price + " apenas na promoção";
+            var texto = string.Format(  "O preço do porduto é {0} apenas na promoção {1}", price, true);
             Console.WriteLine(texto);
         }
     }
