cd9e616e815aa8a56199d10a6bdef9f0181eaf3d
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-11 06:40:11 -0300

    concatenar string

--- Fundação/12_String/Program.cs
+++ Fundação/12_String/Program.cs
@@ -6,8 +6,6 @@
         {
             var price = 400.9;
             var texto = "o preço do produto é " + price + " apenas na promoção";
-            //var texto = string.Format(  "O preço do porduto é {1} apenas na promoção {0}", price, true);
-            //var texto = $"O preço do produto é {price}";
 
             Console.WriteLine(texto);
         }
