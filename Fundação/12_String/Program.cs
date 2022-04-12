6098a1845db3dd57ef8965673badaea2b90b2508
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-12 08:59:50 -0300

    contando string, inserindo e removendo

--- Fundação/12_String/Program.cs
+++ Fundação/12_String/Program.cs
@@ -5,9 +5,11 @@
         static void Main()
         {
             var texto = "Este texto é um teste";
-            Console.WriteLine(texto.IndexOf("é"));  // irá pegar a posição 11 
-            Console.WriteLine(texto.IndexOf("s"));
-            Console.WriteLine(texto.LastIndexOf("s"));
+            Console.WriteLine(texto.ToLower());  // letras em minúscula
+            Console.WriteLine(texto.ToUpper());  // letras em maiúscula
+            Console.WriteLine(texto.Insert(11, "aqui "));  // inserir na posição 11 a palavra aqui
+            Console.WriteLine(texto.Remove(10, 11));  // remover os 11 caracteres após o 10°
+            Console.WriteLine(texto.Length);  // contar os caracteres
         }
     }
 }
