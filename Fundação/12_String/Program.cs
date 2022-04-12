3acf25896916872997dacf544d678b7c1625c42f
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-12 08:39:14 -0300

    String equals

--- Fundação/12_String/Program.cs
+++ Fundação/12_String/Program.cs
@@ -5,15 +5,9 @@
         static void Main()
         {
             var texto = "Este texto é um teste";
-            Console.WriteLine(texto.StartsWith("Este"));  // comparar string no início
-            Console.WriteLine(texto.StartsWith("este"));
-            Console.WriteLine(texto.StartsWith("texte"));        
-
-            Console.WriteLine("------------------");
-
-            Console.WriteLine(texto.EndsWith("teste"));  // comparar string no final 
-            Console.WriteLine(texto.EndsWith("Teste"));
-            Console.WriteLine(texto.EndsWith("xpto"));
+            Console.WriteLine(texto.Equals("Este texto é um teste"));
+            Console.WriteLine(texto.Equals("este texto é um teste"));
+            Console.WriteLine(texto.Equals("este texto é um teste", StringComparison.OrdinalIgnoreCase));
         }
     }
 }
