195b1e3e212b2343fa23dfff977437bea6146e0e
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-12 09:54:20 -0300

    string usando append

--- Fundação/12_String/Program.cs
+++ Fundação/12_String/Program.cs
@@ -1,29 +1,19 @@
-﻿namespace String
+﻿using System.Text;
+
+namespace String
 {
     static class Program
     {
         static void Main()
         {
-            var texto = "Este texto é um teste";
-            Console.WriteLine(texto.Replace("Este", "Isto"));  // trocar Este por Isto
+            var texto = new StringBuilder();  // construtor
+            texto.Append("Este texto é um teste ");
+            texto.Append("e estamos testando ");
+            texto.Append("esses caracteres ");
+            texto.Append("para deixar na mesma linha.");
 
-            Console.WriteLine("---------");
-
-            var divisao = texto.Split(" ");
-            Console.WriteLine(divisao[0]);
-            Console.WriteLine(divisao[1]);
-            Console.WriteLine(divisao[2]);
-            Console.WriteLine(divisao[3]);
-            Console.WriteLine(divisao[4]);
-
-            Console.WriteLine("---------");
-
-            var resultado = texto.Substring(13, 2);  // irá pegar o 13° índice e ira retornar 2 caracteres (um)
-            Console.WriteLine(resultado);
-
-            Console.WriteLine("---------");
-
-            Console.WriteLine(texto.Trim());  // limpa os espaços no início e no final
+            Console.Clear();
+            Console.WriteLine(texto);
         }
     }
 }
