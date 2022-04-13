f5c436eee2a3067fcad6d76526004ce2a4773b82
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-13 09:44:00 -0300

    criando pequeno menu

--- Projetos/EditorHtml/Menu.cs
+++ Projetos/EditorHtml/Menu.cs
@@ -6,10 +6,37 @@ namespace EditorHtml
         { 
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Blue;
-            Console.ForegroundColor = ConsoleColor.Black;       
+            Console.ForegroundColor = ConsoleColor.Black;   
+
+            DrawScreen();
+            Pipe();
+            DrawScreen();
         }
 
+        public static void DrawScreen() 
+        {
+            Console.Write("+");
+            for(int i = 0; i <= 30; i++)
+                Console.Write("-");
+                
+            Console.Write("+");
+            Console.Write("\n");
+        }
 
+        public static void Pipe()
+        {
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+
+                for(int i = 0; i <= 30; i++)  // obs. remover for aninhado.
+                    Console.Write(" ");
+
+                Console.Write("|");
+                Console.Write("\n");
+
+            }
+        }
 
     }
     
