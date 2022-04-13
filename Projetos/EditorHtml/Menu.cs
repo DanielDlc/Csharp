e351fae0f909db4d6f7b4d4d4470ef0e17e9032d
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-13 09:59:06 -0300

    menu de opções

--- Projetos/EditorHtml/Menu.cs
+++ Projetos/EditorHtml/Menu.cs
@@ -8,6 +8,14 @@ namespace EditorHtml
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;   
 
+            Lines();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
+        }
+
+        public static void Lines()
+        {
             DrawScreen();
             Pipe();
             DrawScreen();
@@ -36,6 +44,30 @@ namespace EditorHtml
                 Console.Write("\n");
 
             }
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3,2);
+            Console.WriteLine("Editor HTML");
+
+            Console.SetCursorPosition(3,3);
+            Console.WriteLine("=============");
+
+            Console.SetCursorPosition(3,4);
+            Console.WriteLine("Selecione uma opção abaixo");
+
+            Console.SetCursorPosition(3,6);
+            Console.WriteLine("1 - Novo Arquivo");
+
+            Console.SetCursorPosition(3,7);
+            Console.WriteLine("2 - Abrir");
+
+            Console.SetCursorPosition(3,9);
+            Console.WriteLine("0 - Sair");
+
+            Console.SetCursorPosition(3,10);
+            Console.Write("Opção");
         }
 
     }
