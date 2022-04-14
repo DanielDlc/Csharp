1bf147bbf79c6a0f2b0b8fed4172f10cb3fb8a30
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-14 07:28:39 -0300

    mostrando data atual

---
+++ Fundação/13_Datas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Datas
+{
+    class Program
+    {
+        static void Main (String[] args)
+        {
+            Console.Clear();
+            // var data = new DateTime();
+            var data = DateTime.Now;
+            Console.WriteLine(data);
+        }
+    }
+}
