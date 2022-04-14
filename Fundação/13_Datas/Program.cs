8aec0d2562b068870705f8b383938028909fdb99
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-14 07:38:18 -0300

    obtendo valores de data

--- Fundação/13_Datas/Program.cs
+++ Fundação/13_Datas/Program.cs
@@ -7,9 +7,17 @@ namespace Datas
         static void Main (String[] args)
         {
             Console.Clear();
-            // var data = new DateTime();
-            var data = DateTime.Now;
+            var data = new DateTime(2022, 04, 14, 7, 26, 11);
+            //var data = DateTime.Now;
             Console.WriteLine(data);
+            Console.WriteLine(data.Year);
+            Console.WriteLine(data.Month);
+            Console.WriteLine(data.Day);
+            Console.WriteLine(data.Hour);
+            Console.WriteLine(data.Minute);
+            Console.WriteLine(data.Second);
+
+            Console.WriteLine(data.DayOfYear);
         }
     }
 }
