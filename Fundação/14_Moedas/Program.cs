10ae941d013a62d4e18e42e34ac6f881a1cd9d02
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-15 11:13:11 -0300

    arredondando valor

--- Fundação/14_Moedas/Program.cs
+++ Fundação/14_Moedas/Program.cs
@@ -9,13 +9,14 @@ namespace Moedas
         {
             Console.Clear();
 
-            decimal valor = 10.25m;
-            Console.WriteLine(
-                valor.ToString(
-                    "C",
-                    CultureInfo.CreateSpecificCulture("pt-BR") 
-                )
-            );
+            decimal valor = 400.08m;
+            Console.WriteLine(valor.ToString("C",CultureInfo.CreateSpecificCulture("pt-BR")));
+
+            Console.WriteLine("---------");
+
+            Console.WriteLine(Math.Round(valor));
+            Console.WriteLine(Math.Ceiling(valor));
+            Console.WriteLine(Math.Floor(valor));
         }
     } 
 }
