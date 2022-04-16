2b25e9de88e5fdb480f130b385a9312e60a2bd64
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-16 09:30:02 -0300

    percorrer arrays

---
+++ Fundação/15_Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var meuArray = new int[5]{1, 2, 3, 4, 5}; 
+            meuArray[0] = 400;  // trocar o array na posição 0( que é 1) por 400
+
+            for (var index = 0; index < meuArray.Length; index++)
+            {
+                Console.WriteLine(meuArray[index]);
+            }
+        }
+    }
+}
