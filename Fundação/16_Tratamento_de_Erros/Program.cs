bedd77ab43b656a4f1bef9f69bd8d174ed236170
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-17 11:03:20 -0300

    disparar excessões

--- Fundação/16_Tratamento_de_Erros/Program.cs
+++ Fundação/16_Tratamento_de_Erros/Program.cs
@@ -12,11 +12,13 @@ class Program
 
         try
         {
-            for (var index =0; index < 10; index++)
-            {
-            // System.IndexOutOfRangeException
-            Console.WriteLine(arr[index]);
-            }
+            // for (var index =0; index < 10; index++)
+            // {
+            // // System.IndexOutOfRangeException
+            // Console.WriteLine(arr[index]);
+            // }
+
+            Cadastrar("");
         }
 
         // tratar o erro específico 
@@ -27,12 +29,28 @@ class Program
             Console.WriteLine("Não encontrei o índice na lista");
         }
 
+        // tratar null
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine(ex.InnerException);
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Falha ao cadastrar o texto");
+        }
+
         // tratar outros tipos de erros
         catch (Exception ex)
         {   
             Console.WriteLine(ex.InnerException);
             Console.WriteLine(ex.Message);
             Console.WriteLine("Ops, algo deu errado!");
+        }
+    }
+
+    static void Cadastrar(string texto)
+    {
+        if (string.IsNullOrEmpty(texto))
+        {
+            throw new ArgumentNullException("O texto não pode ser nulo ou vazio");
         }
     }
 }
