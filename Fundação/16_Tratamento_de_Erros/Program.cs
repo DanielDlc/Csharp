dcc0b10bd14def22befb3fb49ee03171a4e8ac02
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-17 10:12:42 -0300

    iniciando tratamento de erros com array de 3 posições

---
+++ Fundação/16_Tratamento_de_Erros/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exceptions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        var arr = new int[3];
+        for (var index =0; index < 10; index++)
+        {
+            // System.IndexOutOfRangeException
+            Console.WriteLine(arr[index]);
+        }
+    }
+}
