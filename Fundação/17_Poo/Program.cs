d1ec312852313951eeb14eb124415f313119c082
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-19 09:43:10 -0300

    criando class com propriedades e métodos

---
+++ Fundação/17_Poo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Poo
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            // um objeto sempre é um tipo de referência
+            // -> armazena apeas o endereço dos dados.
+            Console.WriteLine("Olá, Mundo de Poo!");
+        }
+    }
+
+    class Pagamento
+    {
+        // Propriedades
+        DateTime Vencimento;
+
+        // Métodos
+        void Pagar(){}
+
+    }
+}
