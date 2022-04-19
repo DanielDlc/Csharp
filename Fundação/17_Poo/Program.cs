9380f6fc96fdf658dbfd2bcd9e8d3521e0f89655
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-19 10:46:05 -0300

    herança

--- Fundação/17_Poo/Program.cs
+++ Fundação/17_Poo/Program.cs
@@ -5,29 +5,36 @@
         static void Main(String[] args)
         {
             Console.Clear();
+            var pagamentoBoleto = new PagamentoBoleto();
+            pagamentoBoleto.Pagar();
+            pagamentoBoleto.Vencimento = DateTime.Now;
+            pagamentoBoleto.NumeroBoleto = "123";
+
             // um objeto sempre é um tipo de referência
             // -> armazena apeas o endereço dos dados.
             Console.WriteLine("Classe de Pagamento");
         }
     }
 
+    // classe pai
     class Pagamento
     {
         // Propriedades
-        DateTime Vencimento;
+        public DateTime Vencimento;
 
         // Métodos
-        void Pagar()
-        {
-            ConsultarSaldoDoCartao();
+        public void Pagar() { }
+    }
 
-        }
+    // classe filho 1
+    class PagamentoBoleto : Pagamento  // herança utilizando dois pontos ( pagamento boleto herdando de pagamento)
+    {
+        public string NumeroBoleto;
+    }
 
-        // Abstração: esconder detalhes do cartão 
-        private void ConsultarSaldoDoCartao()
-        {
-
-        }
-
+    // classe filho 2
+    class PagamentoCartaoCredito : Pagamento
+    {
+        public string Numero;
     }
 }
