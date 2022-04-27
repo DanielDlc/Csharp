f5ef2f7696ab1e4af5608fc3b4a78039573e81ee
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-27 19:02:03 -0300

    conteúdo da classe

---
+++ Projetos/CursoOnline/ContentContext/Content.cs
@@ -0,0 +1,11 @@
+namespace CursoOnline.ContentContext
+{
+    public class Content
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+    }
+}
