d1f6ae721b6ea64e88882e53e44cd4061bee011e
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-27 19:11:46 -0300

    criando Guid que indica um Spof

--- Projetos/CursoOnline/ContentContext/Content.cs
+++ Projetos/CursoOnline/ContentContext/Content.cs
@@ -2,7 +2,13 @@ namespace CursoOnline.ContentContext
 {
     public class Content
     {
-        public int Id { get; set; }
+        public Content()
+        {
+            // criando Id no construtor da classe Pai. indicando um Spof
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
 
         public string Title { get; set; }
 
