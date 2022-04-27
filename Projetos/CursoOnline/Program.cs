db3c8851826a8380e5452fbcaeef61b6cf1626ba
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-27 19:00:25 -0300

    criando curso online com Poo

---
+++ Projetos/CursoOnline/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CursoOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var course = new Course();
+        }
+
+    }
+}
