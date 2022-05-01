97bbc11aa664d5cbbc36912620e32e711b827851
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-01 17:25:18 -0300

    Course

--- Projetos/CursoOnline/ContentContext/Course.cs
+++ Projetos/CursoOnline/ContentContext/Course.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+
 namespace CursoOnline.ContentContext
 {
     public class Course : Content
     {
-
+        public Course()
+        {
+            Modules = new List<Module>();
+        }
+        public string Tag { get; set; }
+        public IList<Module> Modules { get; set; }
+        public object Level { get; internal set; }
     }
 }
