3d2397b7adca79a39789f396dc4dccd1f39b1e7a
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-04 07:23:15 -0300

    Lecture

--- Projetos/CursoOnline/ContentContext/Lecture.cs
+++ Projetos/CursoOnline/ContentContext/Lecture.cs
@@ -6,19 +6,6 @@ namespace CursoOnline.ContentContext
     {
         public int Ordem { get; set; }
         public string? Title { get; set; }
-
-        private int durationInMinutes;
-
-        public int GetDurationInMinutes()
-        {
-            return durationInMinutes;
-        }
-
-        public void SetDurationInMinutes(int value)
-        {
-            durationInMinutes = value;
-        }
-
         public int DurationInMinutes { get; set; }
         public EcontentLevel Level { get; set; }
     }
