4c21c0bd7717d809276d1c92d892083aee7c8495
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-05 08:21:42 -0300

    program interagindo com las listas

--- Projetos/CursoOnline/Program.cs
+++ Projetos/CursoOnline/Program.cs
@@ -6,13 +6,31 @@ namespace CursoOnline
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá, Mundo Poo");
-            var course = new Course();
-            course.Level = ContentContext.Enums.EContentLevel.Beginner;
-            foreach (var item in course.Modules)
-            {
+            var articles = new List<Article>();
+            articles.Add(new Article("Artigo sobre OOP", "orientacao-objetos"));
+            articles.Add(new Article("Artigo sobre C#", "csharp"));
+            articles.Add(new Article("Artigo sobre .NET", "dotnet"));
 
+            foreach ((var) article in articles)
+            {
+                Console.WriteLine(article.Id);
+                Console.WriteLine(article.Title);
+                Console.WriteLine(article.Url);
             }
+
+            var courses = new List<Course>();
+            var courseOOP = new Course("Fundamentos OOP", "fundamentos-oop");
+            var courseCsharp = new Course("Fundamentos C#", "fundamentos-csharp");
+            var courseAspNet = new Course("Fundamentos ASP.NET", "fundamentos-aspnet");
+            courses.Add(courseOOP);
+            courses.Add(courseCsharp);
+            courses.Add(courseAspNet);
+
+            var careers = new List<Career>();
+            var careerDotnet = new Career("Especialista .NEt", "especialista-dotnet");
+            var carrerItem = new CareerItem(1,"Comece por aqui", "", null);
+            careerDotnet.Items.Add(careerItem);
+            careers.Add(careerDotnet);
         }
 
     }
