c148532271630a678c7a37d7763afbd3ad0f9771
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-05 08:21:55 -0300

    career

--- Projetos/CursoOnline/ContentContext/Career.cs
+++ Projetos/CursoOnline/ContentContext/Career.cs
@@ -2,6 +2,14 @@ namespace CursoOnline.ContentContext
 {
     public class Career : Content
     {
-        public int Courses { get; set; }
+        public Career(string title, string url)
+            : base(title, url)
+
+        {
+            Items = new List<CareerItem>();
+        }
+
+        public IList<CareerItem> Items { get; set; }
+        public int TotalCourses => Items.Count;
     }
 }
