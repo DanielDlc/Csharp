b27140508c04a428f333be129c91f1fb92539757
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-06 06:20:14 -0300

    interagindo e ordenando as listas

--- Projetos/CursoOnline/Program.cs
+++ Projetos/CursoOnline/Program.cs
@@ -11,12 +11,14 @@ namespace CursoOnline
             articles.Add(new Article("Artigo sobre C#", "csharp"));
             articles.Add(new Article("Artigo sobre .NET", "dotnet"));
 
+            /* comentando esta parte para testar os curos.
             foreach (var article in articles)
             {
                 Console.WriteLine(article.Id);
                 Console.WriteLine(article.Title);
                 Console.WriteLine(article.Url);
             }
+            */
 
             var courses = new List<Course>();
             var courseOOP = new Course("Fundamentos OOP", "fundamentos-oop");
@@ -27,15 +29,20 @@ namespace CursoOnline
             courses.Add(courseAspNet);
 
             var careers = new List<Career>();
-            var careerDotnet = new Career("Especialista .NEt", "especialista-dotnet");
-            var carrerItem = new CareerItem(1,"Comece por aqui", "", null);
+            var careerDotnet = new Career("Especialista .NET", "especialista-dotnet");
+            var careerItem2 = new CareerItem(2,"Aprenda OOP","", null);
+            var careerItem = new CareerItem(1,"Comece por aqui", "", null);
+            var careerItem3 = new CareerItem(3,"Aprenda .NET","", null);
+            careerDotnet.Items.Add(careerItem2);
+            careerDotnet.Items.Add(careerItem3);
             careerDotnet.Items.Add(careerItem);
             careers.Add(careerDotnet);
 
             foreach (var career in careers)
             {
+                Console.Clear();  // removendo warning por enquanto.
                 Console.WriteLine(career.Title);
-                foreach (var item in career.Items)
+                foreach (var item in career.Items.OrderBy(x => x.Order))
                 {
                     Console.WriteLine($"{item.Order}-{item.Title}");
                 }
