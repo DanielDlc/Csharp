b71e086248dd9fe57debd4bf9e6da56f2c03f374
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-07 06:57:42 -0300

    regras de negócio

--- Projetos/CursoOnline/Program.cs
+++ Projetos/CursoOnline/Program.cs
@@ -30,9 +30,9 @@ namespace CursoOnline
 
             var careers = new List<Career>();
             var careerDotnet = new Career("Especialista .NET", "especialista-dotnet");
-            var careerItem2 = new CareerItem(2,"Aprenda OOP","", null);
-            var careerItem = new CareerItem(1,"Comece por aqui", "", null);
-            var careerItem3 = new CareerItem(3,"Aprenda .NET","", null);
+            var careerItem2 = new CareerItem(2,"Aprenda OOP","", courseOOP);
+            var careerItem = new CareerItem(1,"Comece por aqui", "", courseCsharp);
+            var careerItem3 = new CareerItem(3,"Aprenda .NET","", courseAspNet);
             careerDotnet.Items.Add(careerItem2);
             careerDotnet.Items.Add(careerItem3);
             careerDotnet.Items.Add(careerItem);
@@ -45,6 +45,8 @@ namespace CursoOnline
                 foreach (var item in career.Items.OrderBy(x => x.Order))
                 {
                     Console.WriteLine($"{item.Order}-{item.Title}");
+                    Console.WriteLine(item.Course.Title);
+                    Console.WriteLine(item.Course.Level);
                 }
             }
         }
