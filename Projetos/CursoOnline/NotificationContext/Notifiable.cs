ae3624edd736dd63451d45dd872d7dee8f0e8791
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-11 07:43:58 -0300

    public notifiable List

--- Projetos/CursoOnline/NotificationContext/Notifiable.cs
+++ Projetos/CursoOnline/NotificationContext/Notifiable.cs
@@ -3,14 +3,21 @@ namespace CursoOnline.NotificationContext
     public abstract class Notifiable
     {
         public List<Notification>? Notifications { get; set; }
+
+        public Notifiable()
+        {
+            Notifications = new List<Notification>();
+        }
   
-        public void Add(Notification notification)
+        public void AddNotification(Notification notification)
         {
             Notifications.Add(notification);
         }
-        public void AddRange(IEnumerable<Notification> notifications)
+        public void AddNotifications(IEnumerable<Notification> notifications)
         {
             Notifications.AddRange(notifications);
         }
+
+        public bool IsValid => Notifications.Any();
     }
 }
