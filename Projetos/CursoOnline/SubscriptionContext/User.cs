dbdb75509094af941e02006f3a59e0a5f10b6d0f
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-11 07:57:41 -0300

    criando classe usuário

---
+++ Projetos/CursoOnline/SubscriptionContext/User.cs
@@ -0,0 +1,11 @@
+using CursoOnline.SharedContext;
+
+namespace CursoOnline.SubscriptionContext
+{
+    public class User : Base
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+
+}
