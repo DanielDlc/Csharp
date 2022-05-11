aeb28b385e3565b79e42c6f35727f1187686503c
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-11 08:39:56 -0300

    Plan

---
+++ Projetos/CursoOnline/SubscriptionContext/Plan.cs
@@ -0,0 +1,10 @@
+using CursoOnline.SharedContext;
+
+namespace CursoOnline.SubscriptionContext
+{
+    public class Plan : Base
+    {
+        public string? Title { get; set; }
+        public decimal Price { get; set; }
+    }
+}
