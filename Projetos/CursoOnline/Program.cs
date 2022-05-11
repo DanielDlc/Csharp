c502d1d394b978623837b8e62ae8d6d049189076
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-11 07:42:36 -0300

    item notifications

--- Projetos/CursoOnline/Program.cs
+++ Projetos/CursoOnline/Program.cs
@@ -40,13 +40,18 @@ namespace CursoOnline
 
             foreach (var career in careers)
             {
-                Console.Clear();  // removendo warning por enquanto.
+                //Console.Clear();  // removendo warning por enquanto.
                 Console.WriteLine(career.Title);
                 foreach (var item in career.Items.OrderBy(x => x.Order))
                 {
                     Console.WriteLine($"{item.Order}-{item.Title}");
                     Console.WriteLine(item.Course.Title);
                     Console.WriteLine(item.Course.Level);
+
+                    foreach(var notification in item.Notifications)
+                    {
+                        Console.WriteLine($"{notification.Property} - {notification.Message}");
+                    }
                 }
             }
         }
