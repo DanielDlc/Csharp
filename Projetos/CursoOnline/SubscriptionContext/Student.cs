8de38a8284351e80cadc1a1885b0a6a1405ef4e2
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-11 08:40:02 -0300

    Student

--- Projetos/CursoOnline/SubscriptionContext/Student.cs
+++ Projetos/CursoOnline/SubscriptionContext/Student.cs
@@ -7,5 +7,9 @@ namespace CursoOnline.SubscriptionContext
         public string? Name { get; set; }
         public string? Email { get; set; }
         public User? User { get; set; }
+
+        public IList<Subscription>? Subscriptions { get; set; }
+        
+        public bool IsPremium => Subscriptions.Any(x => !x.IsInactive);
     }
 }
